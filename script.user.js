e2d71593fc67a757ca88e645de77b3c7760f9035
Andi <andreasranzenberger@yahoo.de>
2026-05-08 17:26:17 +0200

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -5,7 +5,7 @@
 // @icon          https://play-lh.googleusercontent.com/PuqeuAmOMsDoB9gRCVr-EQHthinCbtaKPzMbxabfmCY9RI9r1fmWncCb4k6umBszzPaszT_o2RopSpIhy9BAiQ=w240-h480-rw
 // @copyright     2026, Andi (Zer089)
 // @license       MIT
-// @version       2.5.50
+// @version       2.5.51
 // @homepage      https://github.com/Zer089/Kleinanzeigen.de-Anzeige_duplizieren_neu_einstellen
 // @updateURL     https://github.com/Zer089/Kleinanzeigen.de-Anzeige_duplizieren_neu_einstellen/raw/main/script.user.js
 // @downloadURL   https://github.com/Zer089/Kleinanzeigen.de-Anzeige_duplizieren_neu_einstellen/raw/main/script.user.js
@@ -125,11 +125,16 @@
            1. ÜBERSICHTSSEITE ("Meine Anzeigen") 
            ---------------------------------------------------- */
         
-        /* Grid Layout vom User erzwungen, falls Tailwind-Klasse bei Kleinanzeigen fehlt (Spalte 2 auf 470px verbreitert) */
+        /* Grid Layout vom User erzwungen, falls Tailwind-Klasse bei Kleinanzeigen fehlt (Spalte 2 auf 670px verbreitert) */
         .custom-ad-grid {
             display: grid !important;
             width: 100% !important;
-            grid-template-columns: 200px 470px auto !important;
+            grid-template-columns: 200px 670px auto !important;
+        }
+
+        /* Margin-Korrektur für die 3. Spalte (Footer-Div) */
+        .custom-ad-grid .mt-xsmall {
+            margin-top: 0px !important;
         }
 
         .is-overview-page ul:has(> li > a[href*="/p-anzeige-bearbeiten.html"]) {
@@ -420,8 +425,8 @@
                                 newFooterDiv.appendChild(footer.firstChild);
                             }
                             
-                            // Neues Grid auf den Haupt-Wrapper anwenden (Spalte 2 auf 470px vergrößert)
-                            mainWrapper.className = "grid w-full grid-cols-[200px_470px_auto] custom-ad-grid";
+                            // Neues Grid auf den Haupt-Wrapper anwenden (Spalte 2 auf 670px vergrößert)
+                            mainWrapper.className = "grid w-full grid-cols-[200px_670px_auto] custom-ad-grid";
                             
                             // Neues DIV als 3. Spalte in den Wrapper verschieben
                             mainWrapper.appendChild(newFooterDiv);

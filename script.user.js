ee95bae22e548d75313e544f30dbb2bbbb284b0d
Andi <andreasranzenberger@yahoo.de>
2026-05-08 20:41:44 +0200

    Update version to 2.5.63 and adjust styles

--- script.user.js
+++ script.user.js
@@ -5,7 +5,7 @@
 // @icon          https://play-lh.googleusercontent.com/PuqeuAmOMsDoB9gRCVr-EQHthinCbtaKPzMbxabfmCY9RI9r1fmWncCb4k6umBszzPaszT_o2RopSpIhy9BAiQ=w240-h480-rw
 // @copyright     2026, Andi (Zer089)
 // @license       MIT
-// @version       2.5.62
+// @version       2.5.63
 // @homepage      https://github.com/Zer089/Kleinanzeigen.de-Anzeige_duplizieren_neu_einstellen
 // @updateURL     https://github.com/Zer089/Kleinanzeigen.de-Anzeige_duplizieren_neu_einstellen/raw/main/script.user.js
 // @downloadURL   https://github.com/Zer089/Kleinanzeigen.de-Anzeige_duplizieren_neu_einstellen/raw/main/script.user.js
@@ -123,11 +123,11 @@
 
         /* Profilbox Anpassungen (Breiten & Abstände) */
         .is-overview-page .userbadges--item { padding-left: 0px !important; }
-        .is-overview-page .ownprofile-main .text-title2.text-onSurfaceSubdued { width: 530px !important; height: 24px !important; }
+        .is-overview-page .ownprofile-main .text-title2.text-onSurfaceSubdued { width: 530px !important; height: 24px !important; margin-bottom: 0px !important; }
         .is-overview-page .ownprofile-badges.userbadges { width: 530px !important; }
         .is-overview-page .UserProfile--Info { width: 530px !important; }
         .is-overview-page .ownprofile-main .mt-xxsmall.flex.flex-col.content-between { row-gap: 8px !important; margin-top: 0px !important; }
-        .is-overview-page ul[data-testid="user-info"] { row-gap: 15px !important; }
+        .is-overview-page ul[data-testid="user-info"] { row-gap: 8px !important; }
         .is-overview-page .user--trx-overview { width: 300px !important; padding-left: 0px !important; }
 
         /* ----------------------------------------------------

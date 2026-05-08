037261deaf362586129e2c88d0fbcaffc019164c
Andi <andreasranzenberger@yahoo.de>
2026-05-08 20:45:31 +0200

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -5,7 +5,7 @@
 // @icon          https://play-lh.googleusercontent.com/PuqeuAmOMsDoB9gRCVr-EQHthinCbtaKPzMbxabfmCY9RI9r1fmWncCb4k6umBszzPaszT_o2RopSpIhy9BAiQ=w240-h480-rw
 // @copyright     2026, Andi (Zer089)
 // @license       MIT
-// @version       2.5.63
+// @version       2.5.64
 // @homepage      https://github.com/Zer089/Kleinanzeigen.de-Anzeige_duplizieren_neu_einstellen
 // @updateURL     https://github.com/Zer089/Kleinanzeigen.de-Anzeige_duplizieren_neu_einstellen/raw/main/script.user.js
 // @downloadURL   https://github.com/Zer089/Kleinanzeigen.de-Anzeige_duplizieren_neu_einstellen/raw/main/script.user.js
@@ -123,11 +123,29 @@
 
         /* Profilbox Anpassungen (Breiten & Abstände) */
         .is-overview-page .userbadges--item { padding-left: 0px !important; }
-        .is-overview-page .ownprofile-main .text-title2.text-onSurfaceSubdued { width: 530px !important; height: 24px !important; margin-bottom: 0px !important; }
+        
+        /* Zwingt den Profilnamen (text-title2) dazu, absolut keinen Abstand nach unten zu haben */
+        .text-title2.text-onSurfaceSubdued,
+        .is-overview-page .ownprofile-main .text-title2.text-onSurfaceSubdued { 
+            width: 530px !important; 
+            height: 24px !important; 
+            margin-bottom: 0px !important; 
+            padding-bottom: 0px !important;
+            line-height: 1 !important;
+        }
+
         .is-overview-page .ownprofile-badges.userbadges { width: 530px !important; }
         .is-overview-page .UserProfile--Info { width: 530px !important; }
         .is-overview-page .ownprofile-main .mt-xxsmall.flex.flex-col.content-between { row-gap: 8px !important; margin-top: 0px !important; }
-        .is-overview-page ul[data-testid="user-info"] { row-gap: 8px !important; }
+        
+        /* fixt die Abstände in der Nutzer-Info (horizontal & vertikal) */
+        .is-overview-page ul[data-testid="user-info"],
+        .m-none.flex.gap-xxlarge.pl-none.pt-large.text-onBackgroundSubdued { 
+            gap: 8px !important; 
+            row-gap: 8px !important; 
+            column-gap: 8px !important;
+        }
+        
         .is-overview-page .user--trx-overview { width: 300px !important; padding-left: 0px !important; }
 
         /* ----------------------------------------------------
